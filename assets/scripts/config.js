324c02c7d19f87e82f06271222d63285c9bc49bf
MBKeeler <mark.keeler@comcast.net>
2017-10-24 19:25:16 -0400

    Adds fix to config.js file
    Removes an uneeded '/' from the production line in the config file.

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -4,7 +4,7 @@ const config = {
   apiOrigins: {
     development: 'http://localhost:4741',
     // production: 'https://ga-wdi-boston.herokuapp.com'
-    production: 'https://floating-river-61399.herokuapp.com/'
+    production: 'https://floating-river-61399.herokuapp.com'
   }
 }
 
